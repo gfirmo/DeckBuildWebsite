38d0eebfa7a176b04f5f53f99c6d64056579c0f3
julesgilligan <61029792+julesgilligan@users.noreply.github.com>
2020-08-28 18:58:34 -0500

    Filter buttons

--- js/importer.js
+++ js/importer.js
@@ -1,68 +1,115 @@
-//var currnum = 0;  //GABRIEL??? Why does currnum still exist?
+fillSelector();
 
 function fillCard(cardID) {
 	d3.csv("js/cgc.csv").then(function(data) {
 	if (data[cardID].Type == "Creature") {
-		document.getElementById('container').innerHTML = document.getElementById('container').innerHTML +  `<div class="grid-item" id="` + cardID + `"onmouseover="showX(this)" onmouseout="hideX(this)">
-		<div class="X" onclick="this.parentNode.remove()"></div>
-		<div style="background-color:GhostWhite;height:92mm;width:66mm;padding:0.1in">
-			<div style="background-color:`+ data[cardID].Color+`; border: 4px solid `+ data[cardID].Color+`;" class="printOmit" id="cName">` + data[cardID].Name + "<div id='cCost' class='printOmit'>" + data[cardID].Cost + "</div> " + `</div>
+		document.getElementById('container').innerHTML += 
+		`<div class="grid-item" id="${cardID}" onmouseover="showX(this)" onmouseout="hideX(this)">
+			<div class="X" onclick="this.parentNode.remove()"> </div>
+			<div style="background-color:GhostWhite;height:92mm;width:66mm;padding:0.1in">
+				<div style="background-color:${data[cardID].Color}; border: 4px solid ${data[cardID].Color};" class="printOmit" id="cName">
+					${data[cardID].Name}
+					<div id="cCost" class="printOmit"> 
+						${data[cardID].Cost}
+					</div>
+				</div>
 				<div style="height:1.6in;">
-				<div id ="cTraits">`+ data[cardID].Traits +`</div> 
+					<div id ="cTraits">
+						${data[cardID].Traits}
+					</div> 
 					<div style="background-color:blue;float:right;height:1.2in;width:0.4in;"> 
-	<div id="cPower">`+ data[cardID].Strength +`</div>
-	<div style="background-color:#8080ff;height:0.4in;border-color:dimgray;border-style:solid;border-width:3px;text-align:center;line-height:20px;" id="cDefense">`+ data[cardID].Health +`</div>
-	<div id="cRestore">`+ data[cardID].Restore +`</div></div>
-<div style="top:116px;" id="cType">`+ data[cardID].Type +`</div> 
-	</div>
-	<div id="cTextBox">`+ data[cardID].Effect.replace(/\n/g, "<br>") +`
-</div>
-	</div> </div>`;
-	// currnum += 1;
+						<div id="cPower">
+							${data[cardID].Strength}
+						</div>
+						<div style="background-color:#8080ff;height:0.4in;border-color:dimgray;border-style:solid;border-width:3px;text-align:center;line-height:20px;" id="cDefense">
+							${data[cardID].Health}
+						</div>
+						<div id="cRestore">
+							${data[cardID].Restore}
+						</div>
+					</div>
+					<div style="top:116px;" id="cType">
+						${data[cardID].Type}
+					</div> 
+				</div>
+				<div id="cTextBox">
+					${data[cardID].Effect.replace(/\n/g, "<br>")}
+				</div>
+			</div> 
+		</div>`;
 	}
 	else if (data[cardID].Type == "Instant") {
-		document.getElementById('container').innerHTML = document.getElementById('container').innerHTML +  `<div class="grid-item" id="` + cardID +`"onmouseover="showX(this)" onmouseout="hideX(this)">
-		<div class="X" onclick="this.parentNode.remove()"></div>
-	<div style="background-color:GhostWhite;height:92mm;width:66mm;padding:0.1in">
-		<div style="background-color:`+ data[cardID].Color+`; border: 4px solid `+ data[cardID].Color+`;" class="printOmit" id="cName">` + data[cardID].Name+ "<div id='cCost' class='printOmit'>" + data[cardID].Cost + "</div> " + `</div>
-			<div style="height:1.6in;">
-			<div id ="cTraits">`+ data[cardID].Traits +`</div> 
-<div style="top:136px;right:95px;" id="cType">`+ data[cardID].Type +`</div> 
-	</div>
-	<div id="cTextBox">`+ data[cardID].Effect.replace(/\n/g, "<br>") +`
-</div>
-	</div> </div>`;
-	// currnum += 1;
+		document.getElementById('container').innerHTML +=  
+		`<div class="grid-item" id="${cardID}" onmouseover="showX(this)" onmouseout="hideX(this)">
+			<div class="X" onclick="this.parentNode.remove()"> </div>
+			<div style="background-color:GhostWhite;height:92mm;width:66mm;padding:0.1in">
+				<div style="background-color:${data[cardID].Color}; border: 4px solid ${data[cardID].Color};" class="printOmit" id="cName"> 
+					${data[cardID].Name}
+					<div id='cCost' class='printOmit'>
+						${data[cardID].Cost}
+					</div>
+				</div>
+				<div style="height:1.6in;">
+					<div id ="cTraits">
+						${data[cardID].Traits}
+					</div> 
+					<div style="top:136px;right:95px;" id="cType">
+						${data[cardID].Type}
+					</div> 
+				</div>
+				<div id="cTextBox">
+					${data[cardID].Effect.replace(/\n/g, "<br>") }
+				</div>
+			</div>
+		</div>`;
+	
 	}
 	else if (data[cardID].Type == "Artifact") {
-		document.getElementById('container').innerHTML = document.getElementById('container').innerHTML +  `<div class="grid-item" id="` + cardID +`"onmouseover="showX(this)" onmouseout="hideX(this)">
-		<div class="X" onclick="this.parentNode.remove()"></div>
-	<div style="background-color:GhostWhite;height:92mm;width:66mm;padding:0.1in">
-			<div style="height:1.6in;">
-			<div id ="cTraits">`+ data[cardID].Traits +`</div>  
-<div style="top:136px;right:95px;" id="cType">`+ data[cardID].Type +`</div>
-<div style="background-color:#8080ff;border-color:dimgray;border-style:solid;border-width:3px;text-align:center;height:0.3in;width:0.3in;position:relative;top:2.8in;left:1.8in;line-height:0px;" id="cDefense">`+ data[cardID].Health +`</div>
-	</div>
-	<div id="cTextBox">`+ data[cardID].Effect.replace(/\n/g, "<br>") +`
-</div>
-<div style="background-color:`+ data[cardID].Color+`; border: 4px solid `+ data[cardID].Color+`;" class="printOmit" id="cName">` + data[cardID].Name+ "<div id='cCost' class='printOmit'>" + data[cardID].Cost + "</div> " + `</div> 
-	</div> </div>`;
-	// currnum += 1;
+		document.getElementById('container').innerHTML +=  
+		`<div class="grid-item" id="${cardID}" onmouseover="showX(this)" onmouseout="hideX(this)">
+			<div class="X" onclick="this.parentNode.remove()"> </div>
+			<div style="background-color:GhostWhite;height:92mm;width:66mm;padding:0.1in">
+				<div style="height:1.6in;">
+					<div id ="cTraits">
+						${data[cardID].Traits}
+					</div>  
+					<div style="top:136px;right:95px;" id="cType">
+						${data[cardID].Type}
+					</div>
+					<div style="background-color:#8080ff;border-color:dimgray;border-style:solid;border-width:3px;text-align:center;height:0.3in;width:0.3in;position:relative;top:2.8in;left:1.8in;line-height:0px;" id="cDefense">
+						${data[cardID].Health}
+					</div>
+				</div>
+				<div id="cTextBox">
+					${data[cardID].Effect.replace(/\n/g, "<br>")}
+				</div>
+				<div style="background-color:${data[cardID].Color}; border: 4px solid ${data[cardID].Color};" class="printOmit" id="cName">
+					${data[cardID].Name}
+					<div id='cCost' class='printOmit'>
+						${data[cardID].Cost}
+					</div>
+				</div> 
+			</div> 
+		</div>`;
 	}
 	});
 }
 
 function fillSelector() {
+	var good_houses = readCheckBoxes();
+	document.getElementById('chosen_card').innerHTML = ""; // wipe old selector
+
 	d3.csv("js/cgc.csv").then(function(data) {
 		var i = 0;
 		try {
-			console.log(data[i].ID);
 			while (data[i].ID != "") {
-				document.getElementById('chosen_card').innerHTML += `<option value="` + data[i].ID + `"> `+ data[i].Name + `</option>`;
+				if (good_houses.includes(data[i].Color)) {
+					document.getElementById('chosen_card').innerHTML += `<option value="${data[i].ID}"> ${data[i].Name} </option>`;
+				}
 				i++;
 			}
 		} catch (error) { // Catches data[i] is out of bounds so the list is empty
-			console.log(i + " cards loaded into the selector!");
+			console.log( document.getElementById('chosen_card').childElementCount + "/" + i + " cards loaded into the selector!");
 		}
 		
 });
@@ -78,11 +125,12 @@ function fillCardSelect() {
 }
 
 function readDList() {
+	
 	var theFile = document.getElementById('myFile').files[0];
 	var fileCont = ""
 	var reader = new FileReader();
     reader.onload = function (evt) {
-		console.log(evt.target.result);
+		document.querySelector(".grid-container").innerHTML = "";
         fileCont = evt.target.result;
 		console.log(fileCont);
 		data = d3.csvParse(fileCont);
@@ -135,9 +183,24 @@ function expCardList() {
     hiddenElement.download = 'decklist.csv';
     hiddenElement.click();
 }
-fillSelector();
 
-function toggleScroll(){
+function readCheckBoxes() {
+	var checked = document.querySelectorAll("input[type=checkbox]:checked");
+	var ret = [];
+	for (box of checked){
+		var house = box.id.split("-")[1]; // Based on how IDs are structured
+		ret.push(house);
+	}
+
+	return ret;
+}
+
+function getCSVCardHouse(card) {
+	var cost = (card.Cost).split(""); //Check periodically against side effects
+	return cost.pop();
+}
+
+function toggleScroll() {
 	if (document.getElementById("scroll_open").style.height == "30px") {
 		document.getElementById("scroll_open").style.height = "0px";
 	} else {
@@ -145,23 +208,37 @@ function toggleScroll(){
 	}
 }
 
-//JULES TODO:
-// figure out the cards drop down so we can filter by color
+function analyzeCards() {
+	/*
+	Stats to get:
+	Number of cards
+	Number of Creatures, Artifacts, Instants
+	Number in each house
+	*/
+	var card_list = document.querySelectorAll(".grid-item");
+	var qty = card_list.length
 
+	var type = {"Creature":0, "Artifact":0, "Instant":0};
+	var houses = {"Purple":0, "Gold":0, "Grey":0, "Green":0, "Blue":0, "Red":0};
+	var house_string = "";
 
-/* OLD STUFF. Delete when ready
+	d3.csv("js/cgc.csv").then(function(data) {
+		for (card of card_list) {
+			type[data[card.id].Type]++; //sloppy to do this with direct indexing
+			houses[data[card.id].Color]++;
+		}
+		for ([house, num] of Object.entries(houses)) {
+			if (num > 0) {
+				house_string += num + " in " + house + "\n";
+			}
+		}
 
-function toggleX(el) {
-	var X = el.querySelector(":scope > .X");
-	if (X.style.visibility == "visible") {
-		X.style.visibility = "hidden";
-	} else {
-		X.style.visibility = "visible";
-	}
+		alert(`${qty} total cards
+
+${type["Creature"]} creatures
+${type["Artifact"]} artifacts
+${type["Instant"]} instants
+		
+${house_string}`);
+	});
 }
-
-function remov(el) {
-	var element = el;
-	element.remove();
-}
-*/
